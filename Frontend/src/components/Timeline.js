a2857783ffa86285d48e79e0323ae4c2d8e3786f
Rohan <rohan.kumarjha.cd.mec19@itbhu.ac.in>
2020-12-31 17:52:34 +0530

    timeline

--- Frontend/src/components/Timeline.js
+++ Frontend/src/components/Timeline.js
@@ -2,7 +2,7 @@
 import React, {useEffect, useState} from 'react';
 // import '../../App.css';
 
-export default function Achievements() {
+export  function Achievements() {
 
   const [achievements, setAchievements] = useState([{
     
@@ -14,6 +14,8 @@ export default function Achievements() {
 
 
   }])
+
+  
 
   useEffect(() => {
     fetch('http://localhost:8080/achievements')
@@ -30,6 +32,41 @@ export default function Achievements() {
       <div>
         <h1>{achievement.name}</h1>
         {/* whatever accordingly */}
+      </div>
+      ))}
+    </div>
+  )
+}
+
+
+export  function Events() {
+
+  const [events, setEvents] = useState([{
+    
+    name: '', 
+    date: '',
+    month: '',
+    year: '',
+    day: '',
+    time: '',
+    venue: ''
+  }])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/events')
+    .then(res => {
+      if(res.ok) {
+        return res.json()
+      }
+    }).then(jsonRes => setEvents(jsonRes))
+  })
+
+  return (
+    <div className="container">
+      {events.map(event => (
+      <div>
+        <h1>{event.name}</h1>
+        
       </div>
       ))}
     </div>
